74d3af56712a98c76eb938332cd4cd8fced1b167
Falase Femi <femifalase228@gmail.com>
2023-03-10 02:17:34 +0100

    using redux toolkit to manage the state in the app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,19 @@ import {
   ShoppingBagIcon,
 } from "@heroicons/react/24/outline";
 import logo from "../assets/logo.png";
+import { useDispatch } from "react-redux";
+import { setOpenCart } from "../app/CartSlice";
 
 const Navbar = () => {
   const [navState, setNavState] = useState(false);
+  const dispatch = useDispatch();
+
+  const onCartToggle = () => {
+    dispatch(setOpenCart({
+      cartState: true
+    }))
+  }
+
   const navRef = useRef(null);
 
   const onNavScroll = () => {
@@ -34,7 +44,7 @@ const Navbar = () => {
         className={
           !navState
             ? "absolute top-7 left-0 right-0 opacity-100 z-50"
-            : "fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-50 blur-effect-theme"
+            : "fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-[200] blur-effect-theme"
         }
         initial={{ opacity: 0, y: -100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -53,7 +63,7 @@ const Navbar = () => {
             <motion.img
               src={logo}
               alt="logo"
-              className="w-16 h-auto"
+            className={`w-16 h-auto ${navState && "filter brightness-0"}`}
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.2 }}
@@ -66,19 +76,19 @@ const Navbar = () => {
             transition={{ duration: 0.5, delay: 0.2 }}
           >
             <li className="grid items-center">
-              <MagnifyingGlassIcon className="icon-style" />
+              <MagnifyingGlassIcon className={`icon-style ${navState && "text-slate-900 transition-all duration-300"}`} />
             </li>
             <li className="grid items-center">
-              <HeartIcon className="icon-style" />
+              <HeartIcon className={`icon-style ${navState && "text-slate-900 transition-all duration-300"}`} />
             </li>
             <li className="grid items-center">
               <button
                 type="button"
                 className="border-none outline-none active:scale-110 transition-all duration-300 relative"
               >
-                <ShoppingBagIcon className="icon-style" />
+                <ShoppingBagIcon onClick={onCartToggle} className={`icon-style ${navState && "text-slate-900 transition-all duration-300"}`} />
                 <div
-                  className={`absolute top-4 right-0 bg-white text-slate-900 shadow shadow-slate-100 w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300`}
+                  className={`absolute top-4 right-0 shadow w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300 ${navState ? 'bg-slate-900 text-slate-100 shadow-slate-900' : 'bg-slate-100 text-slate-900 shadow-slate-100'}` }
                 >
                   0
                 </div>
